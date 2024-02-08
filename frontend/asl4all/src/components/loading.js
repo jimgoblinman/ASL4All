9a06044d1a22bd19f00e63865d87c7a81d14f7a4
test1 <jvi135606>
2024-02-08 15:30:33 +0100

    Basic Loading

---
+++ frontend/asl4all/src/components/loading.js
@@ -0,0 +1,14 @@
+// Loading.js
+
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h2>Loading...</h2>
+      {/* You can add a spinning loader animation or any other visual indication here */}
+    </div>
+  );
+};
+
+export default Loading;
