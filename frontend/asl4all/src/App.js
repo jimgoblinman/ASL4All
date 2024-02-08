78d9f30d869d730bbcfd1a98e5bb5112c2f69831
Gregory Reiter <greg1166@outlook.com>
2024-02-08 14:53:40 +0100

    fix init

--- frontend/asl4all/src/App.js
+++ frontend/asl4all/src/App.js
@@ -1,21 +1,19 @@
-import { useLocation } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import Home from "./pages/home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route 
-          path="/" 
-          element={<Home/>} 
-        />
-
-      </Routes>
+      <Router>
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home/>} 
+          />
+        </Routes>
+      </Router>
     </div>
   );
-
 }
- 
+
 export default App;
