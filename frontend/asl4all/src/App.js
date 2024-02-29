ba3c154b931e604f0aea4414f3c026b527ad6977
Gregory Reiter <greg1166@outlook.com>
2024-02-29 14:24:18 +0100

    feat: added loading screen

--- frontend/asl4all/src/App.js
+++ frontend/asl4all/src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <Router>
             <Routes>
-                <Route path='/ASL4All' element={<Home/>} />
+                <Route path='/asl4all' element={<Home/>} />
                 <Route path="/*" element={<Navigate to="/asl4all" />} />
             </Routes>
         </Router>
