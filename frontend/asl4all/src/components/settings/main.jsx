084de34dfbc1e09f77679319c3b0c07d36b72ea2
Nicolas Castillo Thöni <nca135582@stud.gibb.ch>
2024-02-29 13:00:35 +0100

    feat: Added the settings page layout

---
+++ frontend/asl4all/src/components/settings/main.jsx
@@ -0,0 +1,9 @@
+import FontPart from './font'
+import SizePart from './size'
+import ColorPart from './color'
+import OpacityPart from './opacity'
+
+export function Font() { return <FontPart /> }
+export function Size() { return <SizePart /> }
+export function Color() { return <ColorPart /> }
+export function Opacity() { return <OpacityPart /> }
--- frontend/asl4all/src/pages/home.jsx
+++ frontend/asl4all/src/pages/home.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react'
+
 import { Menu } from '../components/components'
 
 import styles from './home.module.css'
 
 export default function Home() {
+    const [word, setWord] = useState('')
+    const [current, setCurrent] = useState('')
+
     return (
         <div className={styles.wrapper}>
             <Menu />
             <div className={styles.media}></div>
             <div className={styles.text_box}>
-                <div>C</div>
-                <p>AC</p>
+                <div className={current ? styles.active : ''}>{current}</div>
+                <p>{word}</p>
             </div>
         </div>
     )
