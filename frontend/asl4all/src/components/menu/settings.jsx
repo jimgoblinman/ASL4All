ee5bb4dea4104dfd6bc1816d0e83930c6ae9891a
David Rodrigues <dro135638@stud.gibb.ch>
2024-03-21 15:33:34 +0100

    merge "

--- frontend/asl4all/src/components/menu/settings.jsx
+++ frontend/asl4all/src/components/menu/settings.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 import { Block, Dropdown } from '../components'
 import { Font, Size, Color, Opacity, QR, Language } from '../settings/main'
@@ -20,29 +19,7 @@ export default function Settings({ open }) {
         <Dropdown title={Translation[Translation.current].minTextSize} content={<Size />} />
         <Dropdown title={Translation[Translation.current].textColor} content={<Color />} />
         <Dropdown title={Translation[Translation.current].opacity} content={<Opacity />} />
-=======
-import { Link } from "react-router-dom";
-import { Block, Dropdown } from "../components";
-import { Font, Size, Color, Opacity, QR } from "../settings/main";
-import PropTypes from "prop-types";
-import styles from "./settings.module.css";
-
-export default function Settings({ open }) {
-  const display = (
-    <>
-      <Dropdown key="font" title={"Font"} content={<Font />} />
-      <Dropdown
-        key="minTextSize"
-        title={"Minimum Text size"}
-        content={<Size />}
-      />
-      <Dropdown key="textColor" title={"Text Color"} content={<Color />} />
-      <Dropdown key="opacity" title={"Opacity"} content={<Opacity />} />
->>>>>>> 7eb5b6adee02cab36d6e1c838a7d3cc492573355
     </>
-  );
-
-<<<<<<< HEAD
     const settings = [
         <Dropdown title={Translation[Translation.current].displaySettings} content={display} />,
         <Dropdown title={Translation[Translation.current].languageSettings} content={<Language />} />,
@@ -62,42 +39,8 @@ export default function Settings({ open }) {
         </div>
     )
 }
-=======
-  const settings = [
-    <Dropdown
-      key="displaySettings"
-      title={"Display Settings"}
-      content={display}
-    />,
-    <Dropdown
-      key="languageSettings"
-      title={"Language Settings"}
-      content={<></>}
-    />,
-    <Dropdown key="qrCode" title={"QR Code"} content={<QR />} />,
-  ];
 
-  return (
-    <div className={`${styles.wrapper} ${open ? styles.open : ""}`}>
-      <h1>Settings</h1>
-      <div className={styles.settings}>
-        <Block key="settings" content={settings} />
-        <Block
-          key="feedback"
-          content={[
-            <Link key="feedbackLink" to="/feedback" className={styles.feedback}>
-              Feedback
-            </Link>,
-          ]}
-        />
-      </div>
-      <div className={styles.donate}>Donate</div>
-    </div>
-  );
-}
 
-// PropTypes validation
 Settings.propTypes = {
   open: PropTypes.bool.isRequired,
 };
->>>>>>> 7eb5b6adee02cab36d6e1c838a7d3cc492573355
--- frontend/asl4all/src/pages/feedback.jsx
+++ frontend/asl4all/src/pages/feedback.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react";  
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { IoMdSend } from "react-icons/io";
