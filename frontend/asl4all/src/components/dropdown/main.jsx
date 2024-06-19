7b9a369cef689cfb2c8a81989d92d8a22b27f4d8
Gregory Reiter <greg1166@outlook.com>
2024-06-19 15:30:31 +0200

    only one settings box can be open, three lines in hamburger menu

--- frontend/asl4all/src/components/dropdown/main.jsx
+++ frontend/asl4all/src/components/dropdown/main.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./main.module.css";
 
-export default function Dropdown({ title, content }) {
-  const [toggle, setToggle] = useState(false);
-
+export default function Dropdown({ title, content, isOpen, onToggle }) {
   return (
-    <div className={`${styles.wrapper} ${toggle ? styles.open : ""}`}>
-      <div onClick={() => setToggle((prev) => !prev)} className={styles.top}>
+    <div className={`${styles.wrapper} ${isOpen ? styles.open : ""}`}>
+      <div onClick={onToggle} className={styles.top}>
         <p>{title}</p>
         <span></span>
       </div>
       <div className={styles.content}>{content}</div>
     </div>
-  ); 
+  );
 }
 
 Dropdown.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.node.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
 };
