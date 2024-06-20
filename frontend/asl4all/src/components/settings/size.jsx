cfd3322172a29ea2562cbc1f801d5c4b2dcfd5d0
Your Name <you@example.com>
2024-06-20 08:44:41 +0200

    fix: changes to settings gui size and recognition

--- frontend/asl4all/src/components/settings/size.jsx
+++ frontend/asl4all/src/components/settings/size.jsx
@@ -15,16 +15,17 @@ export default function Size() {
       <div className='bg-black/75 p-5 rounded-[0.7rem] bg-blur-md min-w-0 w-full max-w-md'>
         <label>{`${slider}pt `}</label>
         <div className='flex justify-center items-center max-w-full w-full'>
-          <span>12</span>
+          <span>18</span>
           <input
             type='range'
-            min='12'
-            max='48'
+            min='18'
+            max='26'
+            step='0.5'
             value={slider}
             onChange={(e) => setSlider(e.target.value)}
             className='slider mx-2 py-0 w-full min-w-0'
           ></input>
-          <span>48</span>
+          <span>26</span>
         </div>
       </div>
     </div>
